4d084c075a259cbc75fd835686a9563f1e9b230f
qamarabdiaziz <tushez98@gmail.com>
2023-05-23 05:09:12 +0300

    completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre, :turn_page
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+
+    end
+
+end
 
